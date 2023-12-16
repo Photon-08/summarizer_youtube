526343a564e3b989aa6755738f784184c678d9b9
INDRANIL BHATTACHARYYA <125260412+Photon-08@users.noreply.github.com>
2023-12-17 02:09:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def summarize():
     transcript = audio_to_text()
     from transformers import pipeline
 
-    summarizer = pipeline("summarization", model="pszemraj/long-t5-tglobal-base-16384-book-summary"")
+    summarizer = pipeline("summarization", model="pszemraj/long-t5-tglobal-base-16384-book-summary")
 
     
     #print(summarizer(transcript, do_sample=False))
