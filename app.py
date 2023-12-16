e5e99f6af9677b2579a20f4dc869a47a10f1b044
INDRANIL BHATTACHARYYA <125260412+Photon-08@users.noreply.github.com>
2023-12-17 02:28:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -108,12 +108,13 @@ def summarize():
     for i in range(chunks):
         if i == 0:
             tran_text = transcript[:512]
-            inter_output = summarizer(tran_text, do_sample=False)
+            inter_output = summarizer(tran_text, do_sample=False)[0][''summary_text']
             final_output += inter_output
             final_output += ' '
 
         else:
             tran_text = transcript[cutoff:cutoff*2]
+            inter_output = summarizer(tran_text, do_sample=False)[0][''summary_text']
             final_output += inter_output
             final_output += ' '
             cutoff = cutoff*2
